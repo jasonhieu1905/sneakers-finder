e068d7a1ff49f23c5b63ea2767ea4483e0b6428c
Hieu Doan <hieu.doan@officeatwork.com>
2021-07-07 22:18:09 +0700

    set up repository

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Container } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <form className={classes.root} noValidate autoComplete="off">
+        <div>
+          <TextField
+            label="Size"
+            id="standard-size-small"
+            defaultValue="Nike, Adidas, Saucony ..."
+            size="small"
+          />
+        </div>
+      </form>
+    </Container>
   );
 }
 
